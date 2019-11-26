18408f0da7ddb058a1d473261139e69eef88cf7c
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2019-11-26 10:52:58 +0200

    DateSet up and running

---
+++ LunchBox/Model/FoodCategory.swift
@@ -0,0 +1,15 @@
+//
+//  FoodCategory.swift
+//  LunchBox
+//
+//  Created by adonis rumbwere on 21/11/2019.
+//  Copyright © 2019 Adonis Rumbwere. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
+
---
+++ LunchBox/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  LunchBox
+//
+//  Created by adonis rumbwere on 21/11/2019.
+//  Copyright © 2019 Adonis Rumbwere. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
