f5edd61446abee29f3ee18cf50ee837c226b6c9a
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2019-11-27 23:05:10 +0200

    Receipe Selection VC iP

---
+++ LunchBox/Controller/ReceipesSelectionVC.swift
@@ -0,0 +1,21 @@
+//
+//  ReceipesSelectionVC.swift
+//  LunchBox
+//
+//  Created by adonis rumbwere on 27/11/2019.
+//  Copyright © 2019 Adonis Rumbwere. All rights reserved.
+//
+
+import UIKit
+
+class ReceipesSelectionVC: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
